1a6147dc56e8e634022e26e5d2d3eebc2fa0e4c1
David Aronchick <aronchick@gmail.com>
2020-05-31 19:04:04 +0000

    fixing flake for tests

--- test_utils.py
+++ test_utils.py
@@ -4,14 +4,13 @@ import unittest
 import mlspeclib
 from mlspeclib import MLObject
 from pathlib import Path
-from mlspeclib import MLObject
 
 if Path("src").exists():
     sys.path.append(str(Path("src")))
 sys.path.append(str(Path.cwd()))
 sys.path.append(str(Path.cwd().parent))
 
-from utils.utils import KnownException, report_found_params, raise_schema_mismatch
+from utils.utils import KnownException, report_found_params, raise_schema_mismatch  # noqa
 
 
 class UtilsTest(unittest.TestCase):
